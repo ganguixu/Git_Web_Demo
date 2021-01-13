363d9b1ab645af86445a74a7971f143a07f027fc
ganguixu <ganguixu1994@163.com>
2021-01-14 01:31:23 +0800

    master分支更改了loginService

--- webdemo/src/com/ganguixu/service/loginService.java
+++ webdemo/src/com/ganguixu/service/loginService.java
@@ -1,4 +1,10 @@
 package com.ganguixu.service;
 
 public class loginService {
+
+    public String login(String uname,String upass){
+
+        System.out.println("service类执行了");
+        return null ;
+    }
 }
